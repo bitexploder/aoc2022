b29fd378f3922f96247026e8f1131585b3916c65
Jeremy <jeremy.allen@carvesystems.com>
2022-12-06 15:45:30 -0700

    much yak shaving on day6 playing with bit-set crate

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -1,3 +1,4 @@
+use bit_set::BitSet;
 use itertools::Itertools;
 use std::error::Error;
 use std::fs;
@@ -53,6 +54,64 @@ fn find_unique_window_posv2(winsize: usize, comm: &str) -> usize {
     windows.len() + winsize
 }
 
+fn find_unique_window_posv3(winsize: usize, comm: &str) -> i32 {
+    let mut unique_pos: i32 = -1;
+    let mut bits = BitSet::new();
+    let cbytes = comm.as_bytes();
+
+    for (i, _) in cbytes.iter().enumerate() {
+        if i > winsize - 1 {
+            let window = &cbytes[i - winsize..i];
+
+            for c in window {
+                bits.insert((c - 97) as usize);
+            }
+
+            if bits.len() == winsize {
+                unique_pos = (i + 1) as i32;
+                break;
+            }
+
+            //print!("{}:[{:?}]:[{:?}] ", i, window, cbytes);
+        }
+        bits.clear();
+    }
+
+    unique_pos
+}
+
+#[test]
+fn test_bitvecs() {
+    let astr: Vec<u8> = vec![10, 11, 12, 10];
+    let winsize: usize = 14;
+    let mut bits = BitSet::new();
+    eprintln!("astr = {:?}", astr);
+
+    // let mut bits = bitvec![0;32];
+    // bits.set(10, true);
+    for b in astr {
+        bits.insert(b as usize);
+    }
+
+    eprintln!("bits = {:?}, len = {:?}", bits, bits.len());
+}
+
+#[test]
+fn test_example_comms3() {
+    let comms: Vec<&str> = TESTCOMMS.split("\n").collect();
+    assert_eq!(5, find_unique_window_posv3(4, &comms[0]));
+    assert_eq!(6, find_unique_window_posv3(4, &comms[1]));
+    assert_eq!(10, find_unique_window_posv3(4, &comms[2]));
+    assert_eq!(11, find_unique_window_posv3(4, &comms[3]));
+    //
+    let comms2: Vec<&str> = TESTCOMMS2.split("\n").collect();
+    assert_eq!(19, find_unique_window_posv3(14, &comms2[0]));
+    assert_eq!(23, find_unique_window_posv3(14, &comms2[1]));
+    assert_eq!(23, find_unique_window_posv3(14, &comms2[2]));
+    assert_eq!(29, find_unique_window_posv3(14, &comms2[3]));
+    assert_eq!(26, find_unique_window_posv3(14, &comms2[4]));
+}
+
 #[test]
 fn test_example_comms2() {
     let comms: Vec<&str> = TESTCOMMS.split("\n").collect();
@@ -88,9 +147,9 @@ fn test_example_comms() {
 
 fn main() -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string("src/puzzle.txt")?;
-    let pos = find_unique_window_posv2(4, contents.as_str());
+    let pos = find_unique_window_posv3(4, contents.as_str());
     eprintln!("pos with window 4 = {:?}", pos);
-    let pos = find_unique_window_posv2(14, contents.as_str());
+    let pos = find_unique_window_posv3(14, contents.as_str());
     eprintln!("pos with window 14 = {:?}", pos);
     Ok(())
 }
